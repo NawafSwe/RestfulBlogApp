d191d616eaeb94961e7343019690a1fca102e13c
nawaf <princenawaf99@hotmail.com>
2020-05-16 17:26:01 +0300

    stting the server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
 /* -------------------- importing packages --------------------*/
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    app = express();
+const  express     = require('express'),
+       bodyParser    = require('body-parser'),
+       cors          = require('cors'),
+       app           = express(),
+       mongoose      = require('mongoose');
 
 /* -------------------- setting up app --------------------*/
-app.use(cors());
 app.set(express.static('public'));
+app.use(cors());
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 
 /* -------------------- establish connection to database --------------------*/
-
+const uri = 'mongodb://localhost/blog_app';
+mongoose.connect(
+  uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, db) => {
+    if (err) console.log('the error is', err);
+    else console.log('successfully connected');
+  }
+);
 
 /* -------------------- creating models --------------------*/
 
@@ -20,5 +34,10 @@ app.set('view engine', 'ejs');
 /* -------------------- app routes --------------------*/
     
     
+/* -------------------- testing server connection --------------------*/
+const port = 3000;
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+ });
 
        
