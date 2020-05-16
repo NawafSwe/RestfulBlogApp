7a0c0ef5f0b499028307d4d16c17b9110783b841
nawaf <princenawaf99@hotmail.com>
2020-05-16 17:09:59 +0300

    stting the server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+/* -------------------- importing packages --------------------*/
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    app = express();
+
+/* -------------------- setting up app --------------------*/
+app.use(cors());
+app.set(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+
+/* -------------------- establish connection to database --------------------*/
+
+
+/* -------------------- creating models --------------------*/
+
+
+/* -------------------- app routes --------------------*/
+    
+    
+
+       
