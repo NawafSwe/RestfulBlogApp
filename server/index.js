e578f7367a52d268338bd19b1048698f7be29c77
nawaf <princenawaf99@hotmail.com>
2020-05-16 18:14:32 +0300

    stting the server

--- server/index.js
+++ server/index.js
@@ -29,15 +29,51 @@ mongoose.connect(
 );
 
 /* -------------------- creating models --------------------*/
+const blogSchema = mongoose.Schema({
+    title: String,
+    image_url: String,
+    body: String,
+    created: {type:Date, default:Date.now}
+});
+const Blog = mongoose.model('blog', blogSchema);
+
+/*
+Blog.create(
+  generate_blog(
+    'ios dev',
+    'https://relevant.software/wp-content/uploads/2020/01/photo-1484417894907-623942c8ee29-1-scaled.jpeg','good bro'
+  )
+);*/
+
+/* -------------------- app  RESTFUL routes --------------------*/
 
 
-/* -------------------- app routes --------------------*/
+app.get('/blogs', (req, res) => { 
+    Blog.find({}, (err, blogs) => {
+        if (err) console.log('something were wrong');
+        else {
+            console.log(blogs);
+            res.render('index', {blogs: blogs});
+        }
+     })
+});
     
     
 /* -------------------- testing server connection --------------------*/
 const port = 3000;
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
- });
+});
+ 
+
+
+/* -------------------- helper functions --------------------*/
+
+ function generate_blog(title, image_url, body){ 
+    return {
+        title: title,
+        image_url: image_url,
+        body: body,}
+}
 
        
